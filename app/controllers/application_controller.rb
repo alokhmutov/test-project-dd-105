243e3c28a14e591770f42464e1dc6efacf97b592
alokhmutov <alokhmutov@gmail.com>
2023-05-20 14:39:17 +0400

    simulate controller action with device_detector

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :req
+
+  def req
+    client = DeviceDetector.new(request.headers['User-Agent'])
+    client.device_name
+    client.device_type
+  end
 end
